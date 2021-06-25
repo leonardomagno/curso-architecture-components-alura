48ef0e2c31f9b9863fb3317221f7ac0046411431
Leonardo Magno Marones Lopes <leonardo.marones@mobicare.com.br>
2021-06-25 14:49:07 -0300

    - Implementando Múltiplos Painéis.

--- app/src/main/java/br/com/alura/technews/ui/activity/NoticiasActivity.kt
+++ app/src/main/java/br/com/alura/technews/ui/activity/NoticiasActivity.kt
@@ -4,32 +4,66 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentTransaction
 import br.com.alura.technews.R
 import br.com.alura.technews.model.Noticia
 import br.com.alura.technews.ui.activity.extensions.transacaoFragment
 import br.com.alura.technews.ui.fragment.ListaNoticiasFragment
 import br.com.alura.technews.ui.fragment.VisualizaNoticiaFragment
+import kotlinx.android.synthetic.main.activity_noticias.*
+
+private const val TAG_FRAGMENT_VISUALIZA_NOTICIA = "visualizaNoticia"
 
 class NoticiasActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_noticias)
-        if(savedInstanceState == null){
+        configuraFragmentPeloEstado(savedInstanceState)
+    }
+
+    private fun configuraFragmentPeloEstado(savedInstanceState: Bundle?) {
+        if (savedInstanceState == null) {
             abreListaNoticias()
+        } else {
+            tentaReabrirFragmentVisualizaNoticia()
         }
+    }
+
+    private fun tentaReabrirFragmentVisualizaNoticia() {
+        supportFragmentManager
+            .findFragmentByTag(TAG_FRAGMENT_VISUALIZA_NOTICIA)?.let { fragment ->
+
+                val argumentos = fragment.arguments
+                val novoFragment = VisualizaNoticiaFragment()
+                novoFragment.arguments = argumentos
+
+                removeFragmentVisualizaNoticia(fragment)
+
+                transacaoFragment {
+                    val container = configuraContainerFragmentVisualizaNoticia()
+                    replace(container, novoFragment, TAG_FRAGMENT_VISUALIZA_NOTICIA)
+                }
+            }
+    }
+
+    private fun FragmentTransaction.configuraContainerFragmentVisualizaNoticia(): Int {
+        if (activity_noticias_container_secundario != null) {
+            return R.id.activity_noticias_container_secundario
+        }
+        addToBackStack(null)
+        return R.id.activity_noticias_container_primario
     }
 
     private fun abreListaNoticias() {
         transacaoFragment {
-            addToBackStack(null)
-            replace(R.id.activity_noticias_container, ListaNoticiasFragment())
+            replace(R.id.activity_noticias_container_primario, ListaNoticiasFragment())
         }
     }
 
     override fun onAttachFragment(fragment: Fragment?) {
         super.onAttachFragment(fragment)
-        when(fragment){
+        when (fragment) {
             is ListaNoticiasFragment -> {
                 configuraListaNoticias(fragment)
             }
@@ -40,8 +74,20 @@ class NoticiasActivity : AppCompatActivity() {
     }
 
     private fun configuraVisualizaNoticia(fragment: VisualizaNoticiaFragment) {
-        fragment.quandoFinalizaTela = this::finish
+        fragment.quandoFinalizaTela = {
+            supportFragmentManager
+                .findFragmentByTag(TAG_FRAGMENT_VISUALIZA_NOTICIA)?.let { fragment ->
+                    removeFragmentVisualizaNoticia(fragment)
+                }
+        }
         fragment.quandoSelecionaMenuEdicao = this::abreFormularioEdicao
+    }
+
+    private fun removeFragmentVisualizaNoticia(fragment: Fragment) {
+        transacaoFragment {
+            remove(fragment)
+        }
+        supportFragmentManager.popBackStack()
     }
 
     private fun configuraListaNoticias(fragment: ListaNoticiasFragment) {
@@ -60,8 +106,8 @@ class NoticiasActivity : AppCompatActivity() {
         dados.putLong(NOTICIA_ID_CHAVE, noticia.id)
         fragment.arguments = dados
         transacaoFragment {
-            addToBackStack(null)
-            replace(R.id.activity_noticias_container, fragment)
+            val container = configuraContainerFragmentVisualizaNoticia()
+            replace(container, fragment, TAG_FRAGMENT_VISUALIZA_NOTICIA)
         }
     }
 
@@ -70,4 +116,5 @@ class NoticiasActivity : AppCompatActivity() {
         intent.putExtra(NOTICIA_ID_CHAVE, noticia.id)
         startActivity(intent)
     }
+
 }
